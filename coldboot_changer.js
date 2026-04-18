ec864197f032b1757f28571a52b0ecd1d0f28ddc
louaipro99 <lpro2023@outlook.com>
2026-04-18 11:13:59 +0100

    Add files via upload

---
+++ coldboot_changer.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("change")
+const file_location = document.getElementById("sp")
+const result = document.getElementById("re")
+
+
+button.addEventListener("click", function(e){
+    console.log(e)
+    const location = file_location.value
+    result.innerHTML = location
+})
